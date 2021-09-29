f2a05f44146368f98da5d2dfa967c6146a420cab
martin kiwala <martinkiwala4@gmail.com>
2021-09-29 12:32:35 +0300

    worked on payments

--- screens/MakePayment.js
+++ screens/MakePayment.js
@@ -1,15 +1,59 @@
-import React, { useState } from "react";
+import {
+  DatePicker,
+  List,
+  Picker,
+  Provider,
+  WhiteSpace,
+} from "@ant-design/react-native";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { FAB, IconButton, TextInput } from "react-native-paper";
+import Toast from "react-native-toast-message";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "../components/Header";
+import { ADD_PAYMENTS, ADD_RENTALS } from "../redux/types";
 import utils from "../utils";
 
 function MakePayment({ navigation }) {
   const [amount, setAmount] = useState(null);
   const [remarks, setRemarks] = useState(null);
+  const [rental_id, setRentalId] = useState([]);
+  const [date, setDate] = useState(null);
+  const ApiManager = useSelector((state) => state.ApiManager);
+  const rentals = useSelector((state) => state.rentals);
+  const dispatcher = useDispatch();
+
+  useEffect(() => {
+    if (!rentals.length) {
+      Promise.all([ApiManager.get("rentals")])
+        .then((response) => {
+          dispatcher({ type: ADD_RENTALS, payload: response.data });
+        })
+        .catch((e) => {});
+    }
+  }, []);
 
   const submit = () => {
-    navigation.goBack();
+    let path = "/payments";
+    let variables = {
+      payment: {
+        amount,
+        remarks,
+        date,
+        rental_id: rental_id[0],
+      },
+    };
+    ApiManager.post(path, variables)
+      .then((response) => {
+        dispatcher({ type: ADD_PAYMENTS, payload: response.data });
+        navigation.goBack();
+      })
+      .catch((error) => {
+        Toast.show({
+          type: "error",
+          text1: error.message,
+        });
+      });
   };
 
   return (
@@ -23,27 +67,76 @@ function MakePayment({ navigation }) {
         style={styles.iconButton}
       />
       <View style={styles.container}>
-        <TextInput
-          label={utils.strings.amount}
-          value={amount}
-          mode="outlined"
-          keyboardType="number-pad"
-          placeholderTextColor={utils.styles.placeholderTextColor}
-          onChangeText={setAmount}
-          style={styles.input}
-          theme={{ colors: { primary: utils.styles.primaryColor } }}
-        />
-        <TextInput
-          label={utils.strings.remarks}
-          value={remarks}
-          onChangeText={setRemarks}
-          mode="outlined"
-          style={styles.text}
-          scrollEnabled={true}
-          returnKeyType="done"
-          blurOnSubmit={true}
-          theme={{ colors: { primary: utils.styles.primaryColor } }}
-        />
+        <Provider>
+          <List>
+            <TextInput
+              label={utils.strings.amount}
+              value={amount}
+              mode="outlined"
+              keyboardType="number-pad"
+              placeholderTextColor={utils.styles.placeholderTextColor}
+              onChangeText={setAmount}
+              style={styles.input}
+              theme={{ colors: { primary: utils.styles.primaryColor } }}
+            />
+          </List>
+          <WhiteSpace size="lg" />
+          <List>
+            <Picker
+              data={rentals.map((rental) => {
+                return {
+                  label: rental.rental_number,
+                  value: rental.id,
+                };
+              })}
+              cols={1}
+              value={rental_id}
+              extra={"Rental"}
+              onChange={setRentalId}
+              okText="OK"
+              dismissText="Close"
+            >
+              <List.Item>Select Rental</List.Item>
+            </Picker>
+          </List>
+          <WhiteSpace size="lg" />
+          <List>
+            <DatePicker
+              value={date}
+              mode="date"
+              defaultDate={new Date()}
+              minDate={new Date()}
+              maxDate={
+                new Date(`${Number(new Date().getFullYear()) + 10}`, 1, 1)
+              }
+              onChange={setDate}
+              format="YYYY-MM-DD"
+              extra={"Payment Date"}
+              locale={{
+                okText: "OK",
+                dismissText: "Close",
+                DatePickerLocale: { year: "", month: "", day: "" },
+              }}
+            >
+              <List.Item>Select Payment Date</List.Item>
+            </DatePicker>
+          </List>
+          <WhiteSpace size="lg" />
+          <List>
+            <TextInput
+              label={utils.strings.remarks}
+              value={remarks}
+              onChangeText={setRemarks}
+              mode="outlined"
+              style={styles.text}
+              scrollEnabled={true}
+              returnKeyType="done"
+              blurOnSubmit={true}
+              theme={{ colors: { primary: utils.styles.primaryColor } }}
+            />
+          </List>
+          <WhiteSpace size="lg" />
+        </Provider>
         <FAB style={styles.fab} icon="check" onPress={() => submit()} />
       </View>
     </>
@@ -71,6 +164,7 @@ const styles = StyleSheet.create({
   text: {
     height: 80,
     fontSize: 16,
+    marginBottom: 20,
   },
   fab: {
     position: "absolute",
--- screens/Payments.js
+++ screens/Payments.js
@@ -1,32 +1,109 @@
-import React from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
-import { FAB, List } from "react-native-paper";
-import { useSelector } from "react-redux";
+import { Card, WhiteSpace, WingBlank } from "@ant-design/react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
+import {
+  FlatList,
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { FAB } from "react-native-paper";
+import Toast from "react-native-toast-message";
+import { useDispatch, useSelector } from "react-redux";
+import { ADD_PAYMENTS } from "../redux/types";
 import utils from "../utils";
 
 export default function Payments({ navigation }) {
   const payments = useSelector((state) => state.payments);
+  const ApiManager = useSelector((state) => state.ApiManager);
+  const [refreshing, setRefreshing] = useState(false);
+  const dispatcher = useDispatch();
+
+  useEffect(() => getPaymentsData(), []);
+
+  const getPaymentsData = () => {
+    let path = "/payments";
+    let variables = {};
+    ApiManager.get(path, variables)
+      .then((response) => {
+        dispatcher({ type: ADD_PAYMENTS, payload: response.data });
+        setRefreshing(false);
+      })
+      .catch((error) => {
+        Toast.show({
+          type: "error",
+          text1: "Failed to Load Payments.",
+        });
+      });
+  };
+
+  const refresh = () => {
+    setRefreshing(true);
+    getPaymentsData();
+  };
 
   return (
     <View style={styles.container}>
-      {payments.length === 0 ? (
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>You don't have any payments!</Text>
-        </View>
-      ) : (
-        <FlatList
-          data={payments}
-          renderItem={({ item }) => (
-            <List.Item
-              title={"test"}
-              description={"remarks"}
-              descriptionNumberOfLines={1}
-              titleStyle={styles.listTitle}
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={refresh} />
+        }
+      >
+        {payments.length === 0 ? (
+          <>
+            <View style={styles.titleContainer}>
+              <Text style={styles.title}>You don't have any payments!</Text>
+            </View>
+          </>
+        ) : (
+          <>
+            <FlatList
+              data={payments}
+              renderItem={({ item }) => (
+                <>
+                  <WingBlank size="lg">
+                    <Card>
+                      <Card.Header
+                        title={item.payment_reference}
+                        thumb={
+                          <FontAwesome5
+                            style={{ marginRight: 10 }}
+                            name="receipt"
+                          />
+                        }
+                      />
+                      <Card.Body>
+                        <View style={{ height: 120 }}>
+                          <Text style={{ marginLeft: 16, marginBottom: 8 }}>
+                            {item.payment_for}
+                          </Text>
+                          <Text style={{ marginLeft: 16, marginBottom: 8 }}>
+                            {moment(item.date).format("DD-MM-YYYY")}
+                          </Text>
+                          <Text style={{ marginLeft: 16, marginBottom: 8 }}>
+                            {item.remarks}
+                          </Text>
+                          <Text style={{ marginLeft: 16, marginBottom: 8 }}>
+                            {`UGX ${item.amount}`}
+                          </Text>
+                          <Text style={{ marginLeft: 16, marginBottom: 0 }}>
+                            {item.payment_by}
+                          </Text>
+                        </View>
+                      </Card.Body>
+                    </Card>
+                  </WingBlank>
+                  <WhiteSpace size="lg" />
+                </>
+              )}
+              keyExtractor={(item) => item.id}
             />
-          )}
-          keyExtractor={(item) => item.id.toString()}
-        />
-      )}
+          </>
+        )}
+      </ScrollView>
       <FAB
         style={styles.fab}
         small
